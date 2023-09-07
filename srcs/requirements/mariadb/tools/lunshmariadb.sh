7d4bbcfc2a3be5e85842c59caae62b91515354fb
Rahhal Rizqy <rarahhal@c3r5p5.1337.ma>
2023-09-07 22:27:02 +0100

    frst push

---
+++ srcs/requirements/mariadb/tools/lunshmariadb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SQL_DATABASE="todo-list"
+SQL_USER="inception"
+SQL_PASSWORD="1337leet"
+SQL_ROOT_PASSWORD="1337leet"
+
+# Start the MySQL service
+service mysql start
+
+# Run MySQL commands
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# Shutdown MySQL
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+# Start MySQL safely
+exec mysqld_safe
+
